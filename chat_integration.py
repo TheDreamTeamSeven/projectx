bd2b2c49b066d542db7940920977e1dc695fae9b
Endote <nobo155@gmail.com>
2024-04-13 18:41:44 +0200

    Chat in Langchain

--- chat_integration.py
+++ chat_integration.py
@@ -2,15 +2,21 @@ import requests
 import json
 from openai import AzureOpenAI
 
+from langchain_core.messages import HumanMessage
+from langchain_openai import AzureChatOpenAI
+
 # Constants
 API_KEY = "73e54f660d6d4c2aa5b9def3c32cac0c"  # Your API key
 AZURE_ENDPOINT = "https://chatintegration.openai.azure.com/"  # Azure endpoint
 max_tokens = 10
-client = AzureOpenAI(
+client = AzureChatOpenAI(
     azure_endpoint=AZURE_ENDPOINT,
     api_key=API_KEY,
-    api_version="2024-02-01"
+    api_version="2024-02-01",
+    deployment_name='testChatIntegration'
 )
+
+context_str = 'You are a SQL query developer. Please create a proper SQL query for the description as given below:\n'
 
 def send_completion_request( prompt):
     """
@@ -21,14 +27,18 @@ def send_completion_request( prompt):
     """
     print('Sending a test completion job')
 
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {API_KEY}'
-    }
+    print(prompt)
 
-    response = client.completions.create(model='testChatIntegration',  prompt=prompt)
-    full_response = prompt + response.choices[0].text
-    print(full_response)
+    prompt = context_str + prompt
+
+    # response = client.completions.create(model='testChatIntegration',  prompt=prompt, temperature=0.1, top_p=1)
+    # full_response = prompt + response.choices[0].text
+    # print(full_response)
+    message = HumanMessage(
+        model='testChatIntegration',
+        content=prompt
+    )
+    full_response = client([message])
     return full_response
 
 
@@ -54,5 +64,5 @@ def get_sql_query_in_json(nl_query):
 nl_query = "Show me all records from employees where age is greater than 30"
 test_query='What is the color of the sky? Please tell me'
 
-result = send_completion_request(test_query)
+result = send_completion_request(nl_query)
 print(result)
