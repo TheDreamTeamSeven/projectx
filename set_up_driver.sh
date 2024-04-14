cb0d182df1e19716ec5908bc7581e490fee1f874
cf-nolszewska <natalia.olszewska@cloudfide.com>
2024-04-14 10:45:39 +0200

    dad

--- set_up_driver.sh
+++ set_up_driver.sh
@@ -6,11 +6,11 @@ then
     exit;
 fi
 
-curl https://packages.microsoft.com/keys/microsoft.asc | tee /etc/apt/trusted.gpg.d/microsoft.asc
+sudo curl https://packages.microsoft.com/keys/microsoft.asc | tee /etc/apt/trusted.gpg.d/microsoft.asc
 
-curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | tee /etc/apt/sources.list.d/mssql-release.list
+sudo curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | tee /etc/apt/sources.list.d/mssql-release.list
 
-apt-get update
+sudo apt-get update
 ACCEPT_EULA=Y apt-get install -y msodbcsql18
 # optional: for bcp and sqlcmd
 ACCEPT_EULA=Y apt-get install -y mssql-tools18
