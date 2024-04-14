432486d2fb9b9e3d10984b676937e6591f59922d
Endote <nobo155@gmail.com>
2024-04-14 12:16:57 +0200

    'test'

--- app.py
+++ app.py
@@ -228,5 +228,5 @@ if __name__ == '__main__':
     except Exception as e:
         print('ERROR')
         # return jsonify({'error': str(e)}), 500
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=5000)
     # socketio.run(app)
