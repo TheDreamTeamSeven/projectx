d367e7dff3ef402a753bb6409d9f8fef09baa7bc
cf-nolszewska <natalia.olszewska@cloudfide.com>
2024-04-14 11:25:08 +0200

    dada

--- set_up_driver.sh
+++ set_up_driver.sh
@@ -11,9 +11,9 @@ curl https://packages.microsoft.com/keys/microsoft.asc | tee /etc/apt/trusted.gp
 curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | tee /etc/apt/sources.list.d/mssql-release.list
 
 apt-get update
-apt-get install -y msodbcsql18
+ACCEPT_EULA=Y apt-get install -y msodbcsql18
 # optional: for bcp and sqlcmd
-apt-get install -y mssql-tools18
+ACCEPT_EULA=Y  apt-get install -y mssql-tools18
 echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
 source ~/.bashrc
 # optional: for unixODBC development headers
